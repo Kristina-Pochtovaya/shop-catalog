7d7bf18baee4314bebc9ac054890a40d096594f4
kpochtovaya <k.pochtovaya@itransition.com>
2021-02-16 18:07:15 +0300

    edit

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import { AppShopCatalog } from './AppShopCatalogComponent';
-
-ReactDOM.render(
-  <AppShopCatalog />,
-  document.getElementById('root'),
-);
--- src/pages/main/footer/components/SocialButtonComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SocialButton = ({ type }) => (
-
-  <link href="" className={type === 'facebook' ? 'fa fa-facebook' : 'fa fa-instagram'} />
-
-);
-
-export default SocialButton;
