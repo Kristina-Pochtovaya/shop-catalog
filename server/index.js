b648d40fa96353fe8e149ae80068e718426bc859
kpochtovaya <k.pochtovaya@itransition.com>
2021-02-23 14:32:55 +0300

    install mysql and connect server to the sql DB catalogItems via sequelize

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { requestTime, logger } from './middleware.js';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import Sequelize from 'sequelize';
 
 const router = express.Router()
 const __dirname = path.resolve();
@@ -12,6 +13,18 @@ const app = express();
 app.use(cors()) 
 app.use(express.json());
 const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+const sequelize = new Sequelize("catalogItemsDB", "Kristina Pochtovaya", "28Kris2021", {
+  dialect: "mysql",
+  host: "localhost"
+});
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'static','index.html'))
